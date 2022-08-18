963754b765ae75d0407d2e3e642bf0382835c3b2
Artem Shnurko <shnurko.artyom@gmail.com>
2022-08-19 00:05:03 +0200

    task 1, 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+// const formInput = document.querySelector('input');
+// const formTextArea = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+populateInputForm();
+const formData = {};
+
+function onFormInput(evt) {
+  evt.preventDefault();
+  formData[evt.target.name] = evt.target.value;
+  console.log(formData);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateInputForm(e) {
+  const savedForm = localStorage.getItem('feedback-form-state');
+
+  if (savedForm) {
+    const parseForm = JSON.parse(savedForm);
+    console.log(parseForm);
+    form.email.value = parseForm.email;
+    form.message.value = parseForm.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
