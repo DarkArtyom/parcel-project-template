1dfff06ab4708735c3b6ecc4b2815f31b3fb96fd
Artem Shnurko <shnurko.artyom@gmail.com>
2022-08-20 15:00:46 +0200

    validation upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,30 +7,21 @@ player.on('play', function () {
   console.log('played the video!');
 });
 
-const onPlay = function (data) {
-  console.log(data);
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+let onPlay = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
 };
+
+let itemFromStorage = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+if (itemFromStorage) {
+  player.setCurrentTime(itemFromStorage);
+}
+
 player.on('play', throttle(onPlay, 500));
 
-const itemFromStorage = localStorage.getItem('videoplayer-current-time');
-const parsedItem = JSON.parse(itemFromStorage);
-//
-
-function playMovie() {
-  if (Object.keys(parsedItem).length != 0) {
-    player
-      .setCurrentTime(parsedItem.seconds)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'RangeError':
-            break;
-          default:
-            break;
-        }
-      });
-  }
-}
-playMovie();
 // localStorage.clear();
